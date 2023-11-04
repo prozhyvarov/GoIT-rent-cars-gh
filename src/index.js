013ea5ab26a59ec612805aef53846464861df180
prozhyvarov <121400043+prozhyvarov@users.noreply.github.com>
2023-11-04 15:29:39 -0600

    update with gh-pages

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
 import './index.css';
+import { App } from 'components/App/App';
+import { Provider } from 'react-redux';
+import { store } from 'redux/store';
+import { BrowserRouter } from 'react-router-dom';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+        <BrowserRouter basename="/goit-rent-cars">
+          <App />
+        </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
---
+++ src/pages/Catalog/Catalog.jsx
@@ -0,0 +1,60 @@
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect, useState } from "react";
+import {
+  selectAmountCars,
+  selectFilteredCars,
+  selectLoading,
+} from 'redux/selectors.js';
+import { fetchCars } from "redux/operation";
+import { clearCarsList } from "redux/carSlice";
+import { clearFilter } from "redux/filterSlice";
+
+import { NotFound } from "components/NotFound/NotFound";
+import { CarsList } from "components/CarsList/CarsList";
+import { SearchForm } from "components/SearchForm/SearchForm";
+import { LoadMoreButton } from 'components/LoadMoreButton/LoadMoreButton';
+import { Container } from "./Catalog.styled";
+import Loader from "components/Loader/Loader";
+
+
+const Catalog = () => {
+  const dispatch = useDispatch();
+  const [page, setPage] = useState(1);
+ const [initialLoad, setInitialLoad] = useState(true);
+  const amount = useSelector(selectAmountCars);
+
+  console.log(amount);
+ const filteredCars = useSelector(selectFilteredCars);
+ const isLoading = useSelector(selectLoading);
+
+ useEffect(() => {
+   if (page === 1 && !initialLoad) {
+     dispatch(clearCarsList());
+     dispatch(clearFilter());
+   }
+
+   if (!initialLoad) {
+     dispatch(fetchCars({ page, limit: 8 }));
+   } else {
+     setInitialLoad(false); 
+   }
+ }, [dispatch, page, initialLoad]); 
+
+ const handleLoadMore = () => {
+   setPage(prevState => prevState + 1);
+ };
+
+  return (
+    <Container>
+      <SearchForm />
+      {isLoading && <Loader />}
+      {filteredCars.length > 0 && <CarsList cars={filteredCars} />}
+      {filteredCars.length === 0 && !isLoading && <NotFound />}
+      {amount !== filteredCars.length && (
+        <LoadMoreButton onClick={handleLoadMore} />
+      )}
+    </Container>
+  );
+};
+
+export default Catalog;
